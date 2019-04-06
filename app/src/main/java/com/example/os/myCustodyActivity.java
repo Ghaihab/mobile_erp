2de8cb37305af63f9d72263eeabca316b9f01ae2
Ghaihab Almuhanna <g.almuhanna@tamkeentech.sa>
2019-04-06 18:10:48 +0300

    wip

--- app/src/main/java/com/example/os/myCustodyActivity.java
+++ app/src/main/java/com/example/os/myCustodyActivity.java
@@ -45,7 +45,7 @@ public class myCustodyActivity extends AppCompatActivity{
             }
         });
 
-//        setPI();
+
     }
 
 
--- app/src/main/java/com/example/os/req_new_custody_Activity.java
+++ app/src/main/java/com/example/os/req_new_custody_Activity.java
@@ -3,18 +3,29 @@ package com.example.os;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class req_new_custody_Activity extends AppCompatActivity {
-
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.req_new_custody);
 
+        TableLayout accpeted_custodies = findViewById(R.id.accepted_custodies);
+        TableRow tableRow = new TableRow(this);
+        TextView textView = new TextView(this);
+        textView.setText("weclome");
+        tableRow.addView(textView);
+        accpeted_custodies.addView(tableRow);
+
 
     }
+
+
+
 
 
 
